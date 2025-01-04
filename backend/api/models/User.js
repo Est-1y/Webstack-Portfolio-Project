47b1867ed987492cabab3aad13ea089d10a08272
Est-1y <133119207+Est-1y@users.noreply.github.com>
2025-01-04 12:29:48 +0300

    Create User.js

---
+++ backend/api/models/User.js
@@ -0,0 +1,13 @@
+// users module
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        email: { type: String, required:true, unique:true },
+        username: {type: String, required:true, unique:true},
+        password: {type: String, required:true, unique:true},
+        isAdmin: {type: String, default: false},
+    }
+);
+
+module.exports = mongoose.model("User", UserSchema);
