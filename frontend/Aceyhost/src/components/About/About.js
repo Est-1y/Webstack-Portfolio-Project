e25c9d28008a0370f5484ef97a39cd50230781ca
esther <terryiana22@gmail.com>
2025-01-17 10:25:47 +0300

    react app upadtaes

--- frontend/Aceyhost/src/components/About/About.js
+++ frontend/Aceyhost/src/components/About/About.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c93be90a6e8ef2711e41014943c8d9a10e844bdee4c5d6ff9804a4152df88dd4
-size 948
+oid sha256:144d97015c8a0312e5bd84577562329f5b5b745820948889f9251f9dfccd78f0
+size 947
